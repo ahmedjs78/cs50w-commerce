bd0d2f36772e62a3b1c3ef73862d3ccb7bd3f0ea
ahmedjs78 <ahmedjs778@gmail.com>
2024-03-27 22:01:53 +0400

    gg

--- auctions/context_processors.py
+++
@@ -1,9 +0,0 @@
-from .models import User,Listings, Category,Bid
-
-
-
-def add_variable_to_context(request):
-    all_catagores = Category.objects.all()
-    return {
-        'testme': 'Hello world!','all_catagores':all_catagores
-    }
